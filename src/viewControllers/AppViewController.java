003de55e7a4cfc12de266dd0e9fc27d68a0e5dac
Tong Wu <malan52.82@gmail.com>
2017-11-29 23:33:45 -0500

    Declair panel name

--- src/viewControllers/AppViewController.java
+++ src/viewControllers/AppViewController.java
@@ -49,18 +49,22 @@ public class AppViewController extends JFrame implements Observer, ActionListene
 	// Make recode of Model
 	private Model theModel;
 
-	// Initial Components
+	// JPanel
 	private JPanel backgroundPanel;
-	private JPanel infoPanel;
-	private JPanel buttonPanel;
-	private JPanel leftEmptyPanel, middlePanel, rightEmptyPanel, bottomEmptyPanel;
+	private JPanel leftPanel;
+	private JPanel middlePanel;
+	private JPanel rightPanel;
+	private JPanel titlePanel;
+	private JPanel contentPanel;
+	private JPanel pageIndexPanel;
 
-	private JLabel theLabel;
-	private JLabel positionLabel;
-	private JButton startButton;
-	private JButton restartButton;
-	private List<JButton> buttonList;
+	// JLabel
+	private JLabel titleLabel;
+	private JLabel storeInfoLabel;
 	
+	// JButton
+	
+	// Image path
 	private static final String BACKGROUND_IMAGE_PATH = "background_image.png";
 
 	/**
@@ -76,15 +80,14 @@ public class AppViewController extends JFrame implements Observer, ActionListene
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension thisScreen = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setSize((int) thisScreen.getWidth() / 2, (int) thisScreen.getHeight() / 2);
-		this.setTitle("Color Game");
+		this.setTitle("Uncle Tetsu Reservation");
 		this.setLocationByPlatform(true);
-		// this methods asks the window manager to position the frame in the
-		// centre of the screen
+		// Center window in the screen
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setLocation(dim.width / 2 - this.getSize().width / 4*3, dim.height / 2 - this.getSize().height / 4*3);
 		this.setResizable(false);
 
-		// Here we set up the GUI. Initialize all the panels.
+		// Initialize all the panels
 		backgroundPanel = new JPanel() {
 			private static final long serialVersionUID = 1L;
 
@@ -92,7 +95,7 @@ public class AppViewController extends JFrame implements Observer, ActionListene
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
 			    try {
-					File pathToFile = new File("background_image.png");
+					File pathToFile = new File(AppViewController.BACKGROUND_IMAGE_PATH);
 					Image bgImage = ImageIO.read(pathToFile);
 					g.drawImage(bgImage, 0, 0, null);
 				} catch (IOException e) {
@@ -100,56 +103,58 @@ public class AppViewController extends JFrame implements Observer, ActionListene
 				}
 			}
 		};
-		infoPanel = new JPanel();
-		buttonPanel = new JPanel();
-		leftEmptyPanel = new JPanel();
-		rightEmptyPanel = new JPanel();
+		leftPanel = new JPanel();
+		rightPanel = new JPanel();
 		middlePanel = new JPanel();
-		bottomEmptyPanel = new JPanel();
+		titlePanel = new JPanel();
+		contentPanel = new JPanel();
+		pageIndexPanel = new JPanel();
+		
 		// Set all panels' background color to white
-		infoPanel.setBackground(Color.WHITE);
-		buttonPanel.setBackground(Color.WHITE);
-		leftEmptyPanel.setBackground(Color.WHITE);
-		rightEmptyPanel.setBackground(Color.WHITE);
+		titlePanel.setBackground(Color.WHITE);
+		contentPanel.setBackground(Color.WHITE);
+		leftPanel.setBackground(Color.WHITE);
+		rightPanel.setBackground(Color.WHITE);
 		middlePanel.setBackground(Color.WHITE);
-		bottomEmptyPanel.setBackground(Color.WHITE);
+		pageIndexPanel.setBackground(Color.WHITE);
 		this.getContentPane().setBackground(Color.WHITE);
+		
 		// Set layout for those panels that will contain other components.
-		infoPanel.setLayout(new BorderLayout());
-		buttonPanel.setLayout(new BorderLayout());
+		titlePanel.setLayout(new BorderLayout());
+		contentPanel.setLayout(new BorderLayout());
 		middlePanel.setLayout(new BorderLayout());
-		bottomEmptyPanel.setLayout(new BorderLayout());
+		pageIndexPanel.setLayout(new BorderLayout());
 		this.getContentPane().setLayout(new BorderLayout());
 		// Set panels' preferred sizes to make GUI looks nice.
-		infoPanel.setPreferredSize(new Dimension(500, 265));
-		buttonPanel.setPreferredSize(new Dimension(580, 100));
-		leftEmptyPanel.setPreferredSize(new Dimension(50, 384));
-		rightEmptyPanel.setPreferredSize(new Dimension(50, 384));
-		bottomEmptyPanel.setPreferredSize(new Dimension(580, 30));
+		titlePanel.setPreferredSize(new Dimension(500, 265));
+		contentPanel.setPreferredSize(new Dimension(580, 100));
+		leftPanel.setPreferredSize(new Dimension(50, 384));
+		rightPanel.setPreferredSize(new Dimension(50, 384));
+		pageIndexPanel.setPreferredSize(new Dimension(580, 30));
 
-		// Initialize theLabel component
-		theLabel = new JLabel();
-		theLabel.setFont(new Font("Helvetica", Font.BOLD, 22));
-		theLabel.setText(theModel.getWelcomeMsg());
-		theLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		// Add theLabel on infoPanel with position.
-		infoPanel.add(theLabel, BorderLayout.CENTER);
+		// Initialize titleLabel component
+		titleLabel = new JLabel();
+		titleLabel.setFont(new Font("Helvetica", Font.BOLD, 22));
+		titleLabel.setText(theModel.getWelcomeMsg());
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		// Add titleLabel on titlePanel with position.
+		titlePanel.add(titleLabel, BorderLayout.CENTER);
 
 		// Initialize startButton component
 		startButton = new JButton("Start");
 		startButton.setEnabled(true);
 		startButton.setPreferredSize(new Dimension(200, 100));
-		// Add startButton on buttonPanel with position
-		buttonPanel.add(startButton, BorderLayout.CENTER);
+		// Add startButton on contentPanel with position
+		contentPanel.add(startButton, BorderLayout.CENTER);
 		
 		// Add three panels on middlePanel with positions
-		middlePanel.add(infoPanel, BorderLayout.NORTH);
-		middlePanel.add(buttonPanel, BorderLayout.CENTER);
-		middlePanel.add(bottomEmptyPanel, BorderLayout.PAGE_END);
+		middlePanel.add(titlePanel, BorderLayout.NORTH);
+		middlePanel.add(contentPanel, BorderLayout.CENTER);
+		middlePanel.add(pageIndexPanel, BorderLayout.PAGE_END);
 
 		// Add three main panels on content pane with positions
-		/*this.getContentPane().add(leftEmptyPanel, BorderLayout.WEST);
-		this.getContentPane().add(rightEmptyPanel, BorderLayout.EAST);
+		/*this.getContentPane().add(leftPanel, BorderLayout.WEST);
+		this.getContentPane().add(rightPanel, BorderLayout.EAST);
 		this.getContentPane().add(middlePanel, BorderLayout.CENTER);*/
 		this.getContentPane().add(backgroundPanel, BorderLayout.CENTER);
 
